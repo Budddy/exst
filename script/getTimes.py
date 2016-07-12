a7b684e6a5f1997fdd67016a5302772920ec162d
Markus Zisser <markus.zisser@gmx.at>
2016-07-12 23:21:36 +0200

    add script for comparing different runs

--- script/getTimes.py
+++ script/getTimes.py
@@ -9,18 +9,19 @@ import time
 import signal
 
 # maxTime - maximum runtime in Seconds per program Execution
-maxTime = 2000
+maxTime = 1000
 
 # programs to test (path to program / name)
-programs = [("./claspfolio-2.2.0/binaries/clasp-3.1.1-x86_64-linux ", "default_clasp"),
-            ("python ./claspfolio-2.2.0/src/claspfolio.py -I ", "default_claspfolio"),
-            ("python ./claspfolio-2.2.0_patched/src/claspfolio.py -I ", "patched_claspfolio")]
+programs = [("./claspfolio-2.2.0/binaries/clasp-3.1.1-x86_64-linux ", "default_clasp")]  # ,
+#            ("python ./claspfolio-2.2.0/src/claspfolio.py --configfile ./claspfolio-2.2.0/models/release2.2/config.json -I ", "default_claspfolio"),
+#            ("python ./claspfolio-2.2.0_patched/src/claspfolio.py --configfile ./claspfolio-2.2.0_patched/models/release2.2/config.json -I ", "patched_claspfolio")]
 
 # path to the folder with the ground test data files
 testData = "./instances"
-
+resultDir = "./result"
+separate = 1
 # number of runs per program/instance
-runs = 2
+runs = 1
 
 
 class Command(object):
@@ -70,33 +71,66 @@ class Command(object):
         return self.status, self.output, self.error, end - start, runCompleted
 
 
-result = open(testData + "/result/_result_", 'a')
+if not isfile(resultDir + "/_result_"):
+    result = open(resultDir + "/_result_", 'w')
+else:
+    result = open(resultDir + "/_result_", 'a')
+
+numElements = len(listdir(testData))
+countElements = 0
+
 for a in listdir(testData):
+    countElements += 1
     f = join(testData, a)
     if isfile(f):
-        print "\ninstance: " + a
+        print ("\ninstance(" + str(numElements) + "/" + str(countElements) + "): " + a)
+        result.write("\ninstance(" + str(numElements) + "/" + str(countElements) + "): " + a + "\n")
+        result.flush()
         for i in programs:
-            print "  program: " + i[1]
-            for count in range(1, runs):
-                com = i[0] + os.path.dirname(f) + "/" + os.path.basename(f)
-                c = Command(com)
-                ret = c.run(timeout=maxTime)
-                # if program takes too long, don't start more runs of the program for this test instance
-                if ret[3] == 1:
-                    print "    could not solve instance within time limit"
-                    fi = open(testData + "/result/" + i[1] + "_failed_" + a, 'w')
-                    fi.write(str(ret[1]))
-                    fi.close()
-                    result.write("    could not solve instance within time limit")
-                    result.flush()
-                    break
-                else:
-                    for s in ret[1].split("\n"):
-                        if "CPU Time" in s:
-                            print "    execution time: " + str(ret[3]) + "      " + s
-                            fi = open(testData + "/result/" + i[1] + "_" + a + "_number_" + str(count), 'w')
-                            fi.write(str(ret[1]))
-                            fi.close()
-                            result.write("    execution time: " + str(ret[3]) + "      " + s)
-                            result.flush()
+            print ("  program: " + i[1])
+            result.write("  program: " + i[1] + "\n")
+            result.flush()
+            for count in range(0, runs):
+                # only run if file doesn't already exist
+                if not isfile(resultDir + i[1] + "_" + a + "_number_" + str(count)):
+                    com = i[0] + os.path.dirname(f) + "/" + os.path.basename(f)
+                    c = Command(com)
+                    ret = c.run(timeout=maxTime)
+                    # if program takes too long, don't start more runs of the program for this test instance
+                    if ret[4] == 0:
+                        print ("    could not solve instance within time limit")
+                        fi = open(resultDir + i[1] + "_failed_" + a, 'w')
+                        fi.write(str(ret[1]))
+                        fi.close()
+                        result.write("    could not solve instance within time limit" + "\n")
+                        result.flush()
+                        break
+                    else:
+                        for s in ret[1].split("\n"):
+                            if "CPU Time" in s:
+                                print ("    execution time: " + str(ret[3]) + "      " + s)
+                                result.write("    execution time: " + str(ret[3]) + "      " + s + "\n")
+                                result.flush()
+                                fi = open(resultDir + i[1] + "_" + a + "_number_" + str(count), 'w')
+                                if separate:
+                                    fi.close()
+                                    if ret[3] <= 1:
+                                        fi = open(resultDir + "/result_1s/" + i[1] + "_" + a + "_number_" + str(count),
+                                                  'w')
+                                    elif ret[3] <= 10:
+                                        fi = open(resultDir + "/result_10s/" + i[1] + "_" + a + "_number_" + str(count),
+                                                  'w')
+                                    elif ret[3] <= 100:
+                                        fi = open(resultDir + "/result_100s/" + i[1] + "_" + a + "_number_" + str(count),
+                                                  'w')
+                                    elif ret[3] <= 1000:
+                                        fi = open(
+                                            resultDir + "/result_1000s/" + i[1] + "_" + a + "_number_" + str(count), 'w')
+                                    else:
+                                        fi = open(resultDir + i[1] + "_" + a + "_number_" + str(count), 'w')
+                                fi.write(str(ret[1]))
+                                fi.flush()
+                                fi.close()
+                                result.write("    execution time: " + str(ret[3]) + "      " + s + "\n")
+                                result.flush()
 result.close()
