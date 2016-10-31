9687de364d4bc010068b3a5aa3c645352a746152
Markus Zisser <markus.zisser@gmx.at>
2016-10-31 10:52:09 +0100

    test travis build

--- test_travis.sh
+++ test_travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/travis/build/Budddy/test_exst/build_htd/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 ./build_test/bin/exst_test
 rc=$?
 if [[ $rc = 1 ]]; then exit 0;else exit 1;fi
